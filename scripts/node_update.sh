61d23e85e93c899a69996bb4a7c8a56187532ad6
ama31337 <70890873+ama31337@users.noreply.github.com>
2020-09-24 12:46:32 +0700

    Update node_update.sh

--- scripts/node_update.sh
+++ scripts/node_update.sh
@@ -19,8 +19,8 @@ if [ $? -ne 0 ]; then
         #if make make was succesfully startup a new node
         if [ $? -eq 0 ]
         then
-#            mv /home/$USER/nearcore /home/$USER/nearcore.bak/nearcore-"`date +"%Y-%m-%d(%H:%M)"`"
-            mv /home/$USER/nearcore /home/$USER/nearcore-"`date +"%Y-%m-%d(%H:%M)"`"
+            mkdir -p /home/$USER/nearcore.bak
+            mv /home/$USER/nearcore /home/$USER/nearcore.bak/nearcore-"`date +"%Y-%m-%d(%H:%M)"`"
             mv /home/$USER/nearcore.new /home/$USER/nearcore
             cd /home/$USER/
             nearup stop
